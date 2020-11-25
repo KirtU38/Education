214f94e14ab9cfe5d2a8497a217bc67a0a547c3f
KirtU38 <Supermanegor1994>
2020-11-25 22:40:17 +0700

    ForkJoin Kinda working

--- (TESTER) Multithreading/Web Crawler/src/main/java/Main.java
+++ (TESTER) Multithreading/Web Crawler/src/main/java/Main.java
@@ -1,21 +1,22 @@
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
-    public static final String path = "src/main/resources/skillbox.txt";
+    public static final String PATH = "src/main/resources/skillbox.txt";
     public static final String ROOT_URL = "https://skillbox.ru/";
+    public static final int NUM_OF_CORES = Runtime.getRuntime().availableProcessors();
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
         int numOfTabs = 0;
-        PrintWriter writer = new PrintWriter(path);
 
-        WebCrawler webCrawler = new WebCrawler();
-        webCrawler.exploreAndWriteURL(ROOT_URL, ROOT_URL, true, numOfTabs, writer);
-        writer.flush();
-        writer.close();
-    }
+        ForkJoinPool pool = new ForkJoinPool(NUM_OF_CORES);
+        WebTask webTask = new WebTask(ROOT_URL, ROOT_URL, true, numOfTabs);
+        List<String> listOfLinks = pool.invoke(webTask);
+        listOfLinks.forEach(System.out::println);
+    } // 439
 }
 
 
